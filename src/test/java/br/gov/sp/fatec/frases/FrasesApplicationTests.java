50c1eae8abf30591329581560550c4c7949a7fd4
Emanuel Mineda Carneiro <mineda@gmail.com>
2021-08-26 16:07:35 -0300

    Commit inicial

---
+++ src/test/java/br/gov/sp/fatec/frases/FrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
